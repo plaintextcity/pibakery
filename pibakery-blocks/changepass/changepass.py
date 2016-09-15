ccc490abc7315b83c654d2908e0c3f22e7413c5e
plaintextcity <plaintextcity@users.noreply.github.com>
2016-09-15 18:45:18 -0400

    Don't hash password if already sha512
    Check if password starts with sha512 "magic number" sequence, only crypt it if it doesn't.  This allows encrypting password before writing to SD card, so exposure of the SD card won't disclose the password which may be shared on other systems.

--- pibakery-blocks/changepass/changepass.py
+++ pibakery-blocks/changepass/changepass.py
@@ -1,17 +1,20 @@
 #!/usr/bin/env python
 # Thanks to Filipe Pina on StackOverflow
+# plaintextcity: use sha512.  Allow sha512 crypts directly.
 
 import subprocess,crypt,random, sys
 
 login = 'pi'
 password = sys.argv[1]
+magic = password[:3]
+print 'magic: ' + magic
 
-ALPHABET = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ./"
-salt = ''.join(random.choice(ALPHABET) for i in range(16))
+if magic != '$6$':
+    salt = ''.join(random.choice(ALPHABET) for i in range(16))
+    salt = 'Saltstring'
+    password = crypt.crypt(password,'$6$'+salt+'$')
 
-shadow_password = crypt.crypt(password,'$6$'+salt+'$')
-
-r = subprocess.call(('usermod', '-p', shadow_password, login))
+r = subprocess.call(('usermod', '-p', password, login))
 
 if r != 0:
     print 'Error changing password for ' + login
